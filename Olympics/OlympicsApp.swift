7bbfddc7a1a6a1ccc89f9cb46c2558c78e2939b1
Clemente <nolasco.clemente1129@gmail.com>
2025-05-23 11:05:54 -0700

    Initial Commit

---
+++ Olympics/OlympicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  OlympicsApp.swift
+//  Olympics
+//
+//  Created by Student1 on 5/23/25.
+//
+
+import SwiftUI
+
+@main
+struct OlympicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
